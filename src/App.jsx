e10b6055ad55b327bd7b6ea3a65feb42c2e138f2
shintarou kawabe <s.kawabe2281@gmail.com>
2020-11-18 23:22:18 +0900

    [add]movie #6 end

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import defaultDataset from "./dataset";
 import './assets/styles/style.css'
-// 関数のimportは{}で囲む
-import {AnswersList} from './components/index'
+// 関数のimportは{}で囲む エントリポイントを用意すると１行で複数コンポーネントがimportできる
+import {AnswersList, Chats} from './components/index'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,28 +14,83 @@ export default class App extends React.Component {
       dataset: defaultDataset,
       open: false
     }
+    this.selectAnswer = this.selectAnswer.bind(this)
   }
 
-  // dataset.jsからinitのanswersを取得
-  initAnswer = () => {
-    const initDataset = this.state.dataset[this.state.currentId];
-    const initAnswers = initDataset.answers;
+  displayNextQuestion = (nextQuestionId) => {
+    const chats = this.state.chats;
+    chats.push({
+      text: this.state.dataset[nextQuestionId].question,
+      type: 'question'
+    })
 
     this.setState({
-      answers: initAnswers
-    });
+      answers: this.state.dataset[nextQuestionId].answers,
+      chats: chats,
+      currentId: nextQuestionId
+    })
   }
+
+  selectAnswer = (selectedAnswer, nextQuestionId) => {
+    switch(true) {
+      case (nextQuestionId === 'init') :
+        this.displayNextQuestion(nextQuestionId);
+        break;
+      default :
+        const chats = this.state.chats;
+        chats.push({
+          text: selectedAnswer,
+          type: 'answer'
+        })
+    
+        this.setState({
+          chats: chats
+        });
+
+        // 回答を送信後、次の質問を表示
+        this.displayNextQuestion(nextQuestionId);
+        break;
+    }
+  }
+
+  // // dataset.jsからinitのanswersを取得
+  // initAnswer = () => {
+  //   const initDataset = this.state.dataset[this.state.currentId];
+  //   const initAnswers = initDataset.answers;
+
+  //   this.setState({
+  //     answers: initAnswers
+  //   });
+  // }
+
+  // initChats = () => {
+  //   const initDataset = this.state.dataset[this.state.currentId];
+  //   const chat = {
+  //     text: initDataset.question,
+  //     type: 'question'
+  //   }
+
+  //   // 一度stateを取得し、新しい配列を作成してからsetStateする
+  //   const chats = this.state.chats;
+  //   chats.push(chat)
+
+  //   this.setState({
+  //     chats: chats
+  //   });
+  // }
 
   // 最初のレンダーが終わったら一度だけinitAnswerを実行してstateを書き換える
   componentDidMount() {
-    this.initAnswer()
+    const initAnswer = "";
+    this.selectAnswer(initAnswer, this.state.currentId);
   }
 
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
-          <AnswersList answers={this.state.answers} />
+          <Chats chats={this.state.chats} />
+          <AnswersList answers={this.state.answers} select={this.selectAnswer} />
         </div>
       </section>
     );
